0d00e75f47305686aed163bc6de424db60e53c22
matsu <tmtmt2209@gmail.com>
2024-12-29 09:07:45 +0000

    chore: nix flake

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "try-dlt-dbt";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            pkgs.uv
+          ];
+          shellHook = ''
+            uv venv
+            source .venv/bin/activate
+          '';
+        };
+        formatter = pkgs.nixfmt-rfc-style;
+      }
+    );
+}
